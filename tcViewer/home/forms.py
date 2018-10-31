4f54fed92f60e43e30d04330d341f5398924d332
byron <zhibohi@163.com>
2018-11-01 00:28:25 +0800

    Update
    Signed-off-by: byron <zhibohi@163.com>

--- tcViewer/home/forms.py
+++ tcViewer/home/forms.py
@@ -2,6 +2,7 @@ from django import  forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from sfxRelease.models import SoftwareRelease, FirmwareRelease, AppRelease
+from tcProject.models import TcProject
 
 
 class SignupForm(UserCreationForm):
@@ -12,11 +13,27 @@ class SignupForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2',)
 
 
-class SoftwareReleaseForm(forms.Form):
-    model = SoftwareRelease
+class SoftwareReleaseForm(forms.ModelForm):
+    class Meta:
+        model = SoftwareRelease
+        fields = ('revision', 'branch', 'rel_date')
 
 
-class FirmwareReleaseForm(forms.Form):
+class FirmwareReleaseForm(forms.ModelForm):
     class Meta:
         model = FirmwareRelease
+        fields = ('version', 'fw_type', 'rel_date')
 
+
+class AppReleaForm(forms.ModelForm):
+    class Meta:
+        model = AppRelease
+        fields = ('version', 'branch', 'rel_date')
+
+
+class TcProjectForm(forms.ModelForm):
+    class Meta:
+        model = TcProject
+        widgets = {'tc_feature': forms.Testarea(attrs={'row': 3, 'cols': 60}), }
+        fields = ('name', 'description', 'tc_feature')
+        
